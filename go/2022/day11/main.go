66412b5fd2bd943da441e34bac3af7a1cee45694
samuel <samuel.recker@powerbot-trading.com>
2022-12-28 11:58:59 +0100

    day11

--- go/2022/day11/main.go
+++ go/2022/day11/main.go
@@ -19,15 +19,8 @@ func main() {
 	fmt.Printf("Day 11 solution B: %d\n", solutionB)
 }
 
-// var re = regexp.MustCompile(`Monkey (?P<src>\d):
-//   Starting items: (?P<items>\d+(?:\s*,\s*\d+)+)
-//   Operation: new = (?P<op>[old+*\/\d]+)
-//   Test: divisible by (?P<test>\d+)
-//     If true: throw to monkey (?P<dst1>\d+)
-//     If false: throw to monkey (?P<dst2>\d+)`)
-
 var re = regexp.MustCompile(`Monkey (?P<id>\d):
-  Starting items: (?P<items>\d+(?:\s*,\s*\d+)+)
+  Starting items: (?P<items>\d+(?:\s*,\s*\d+)*)
   Operation: new = (?P<operation>.+)
   Test: divisible by (?P<divisor>\d+)
     If true: throw to monkey (?P<destinationTrue>\d+)
@@ -36,13 +29,23 @@ var re = regexp.MustCompile(`Monkey (?P<id>\d):
 type monkey struct {
 	id               int
 	items            []int
+	inspectionCount  int
 	expression       govaluate.EvaluableExpression
 	divisor          int
 	destinationTrue  int
 	destinationFalse int
 }
 
-func printMonkeys(monkeys map[int]monkey) {
+func getMonkeyBusiness(monkeys map[int]*monkey) int {
+	keys := make([]int, 0, len(monkeys))
+	for k := range monkeys {
+		keys = append(keys, k)
+	}
+	sort.Slice(keys, func(i, j int) bool { return monkeys[keys[i]].inspectionCount > monkeys[keys[j]].inspectionCount })
+	return monkeys[keys[0]].inspectionCount * monkeys[keys[1]].inspectionCount
+}
+
+func printMonkeys(monkeys map[int]*monkey) {
 	keys := make([]int, 0, len(monkeys))
 	for k := range monkeys {
 		keys = append(keys, k)
@@ -50,7 +53,7 @@ func printMonkeys(monkeys map[int]monkey) {
 	sort.Ints(keys)
 	for _, key := range keys {
 		monkey := monkeys[key]
-		fmt.Printf("\nMonkey #%d:", monkey.id)
+		fmt.Printf("\nMonkey #%d (%3d):", monkey.id, monkey.inspectionCount)
 		for _, item := range monkey.items {
 			fmt.Printf(" %*d", 5, item)
 		}
@@ -58,12 +61,12 @@ func printMonkeys(monkeys map[int]monkey) {
 	fmt.Printf("\n")
 }
 
-func PartA(input string) int {
-	var monkeys map[int]monkey = make(map[int]monkey)
+func buildMonkeys(input string) *map[int]*monkey {
+	var monkeys map[int]*monkey = make(map[int]*monkey)
 	groupNames := re.SubexpNames()
 	for _, match := range re.FindAllStringSubmatch(input, -1) {
-		// fmt.Printf("\nMatch #%d:\n", matchNum)
-		m := monkey{}
+		m := &monkey{}
+		m.inspectionCount = 0
 		for groupIdx, text := range match {
 			group := groupNames[groupIdx]
 			switch group {
@@ -84,41 +87,76 @@ func PartA(input string) int {
 			case "destinationFalse":
 				m.destinationFalse = utils.ToInt(text)
 			}
-			// fmt.Printf("#%d text: '%s', group: '%s'\n", matchNum, text, group)
 		}
-		fmt.Println(m)
 		monkeys[m.id] = m
 	}
+	return &monkeys
+}
 
-	for m := range monkeys {
-		monki := monkeys[m]
-		fmt.Printf("\n")
-		fmt.Printf("Evaluating Monkey #%d\n", monki.id)
-		for i, item := range monkeys[m].items {
+func runRound(monkeys *map[int]*monkey, f func(int) int, verbose bool) {
+	if verbose {
+		fmt.Printf("====== Starting new round ======")
+	}
+	keys := make([]int, 0, len(*monkeys))
+	for k := range *monkeys {
+		keys = append(keys, k)
+	}
+	sort.Ints(keys)
+	for _, m := range keys {
+		monki := (*monkeys)[m]
+		if verbose {
+			fmt.Printf("\n")
+			fmt.Printf("Evaluating Monkey #%d\n", monki.id)
+		}
+		for i, item := range monki.items {
 			parameters := make(map[string]interface{}, 1)
 			parameters["old"] = item
 			result, err := monki.expression.Evaluate(parameters)
 			if err != nil {
 				panic(err)
 			}
-			newVal := int(result.(float64) / 3)
-			// fmt.Printf("\t- item %d -> %d\n", item, newVal)
-			monkeys[m].items[i] = newVal
-			var newMonkey monkey
+			newVal := f(int(result.(float64)))
+			monki.items[i] = newVal
+			dest := 0
 			if (newVal % monki.divisor) == 0 {
-				newMonkey = monkeys[monki.destinationTrue]
+				dest = monki.destinationTrue
 			} else {
-				newMonkey = monkeys[monki.destinationFalse]
+				dest = monki.destinationFalse
 			}
-			newMonkey.items = append(newMonkey.items, item)
+			newMonkey := (*monkeys)[dest]
+			newMonkey.items = append(newMonkey.items, newVal)
+			monki.inspectionCount++
 		}
 		monki.items = make([]int, 0)
-		printMonkeys(monkeys)
+		(*monkeys)[m] = monki
+		if verbose {
+			printMonkeys(*monkeys)
+		}
 	}
+}
 
-	return 0
+func PartA(input string) int {
+	monkeys := buildMonkeys(input)
+	for i := 0; i < 20; i++ {
+		runRound(monkeys, func(i int) int { return i / 3 }, true)
+		printMonkeys(*monkeys)
+	}
+	return getMonkeyBusiness(*monkeys)
+}
+
+func getDivisorProduct(monkeys *map[int]*monkey) int {
+	p := 1
+	for _, m := range *monkeys {
+		p *= m.divisor
+	}
+	return p
 }
 
 func PartB(input string) int {
-	return 0
+	monkeys := buildMonkeys(input)
+	p := getDivisorProduct(monkeys)
+	for i := 0; i < 10000; i++ {
+		runRound(monkeys, func(i int) int { return i % p }, false)
+	}
+	return getMonkeyBusiness(*monkeys)
 }
