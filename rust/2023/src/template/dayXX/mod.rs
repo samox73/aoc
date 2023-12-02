4976ca2b4230a69fa61444f8c581e4d4ac331442
Samuel Recker <samuel.recker@gmail.com>
2023-12-02 10:41:00 +0100

    update setup script

---
+++ rust/2023/src/template/dayXX/mod.rs
@@ -0,0 +1,8 @@
+pub fn solve_a() {
+    let input = aoc_utils::get_input(2023, 02);
+    println!("{}", input);
+}
+
+pub fn solve_b() {
+    let input = aoc_utils::get_input(2023, 02);
+}
--- rust/2023/src/template/mod.rs
+++
@@ -1,4 +0,0 @@
-pub fn solve() {
-    let input = aoc_utils::get_input(2023, DAY);
-    println!("{}", input);
-}
