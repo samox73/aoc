f84bc08045b698debe6ba9ffe3a9903133a3f54a
Samuel Recker <samuel.recker@gmail.com>
2023-12-09 18:22:30 +0100

    rust: 2023/9

--- rust/2023/src/main.rs
+++ rust/2023/src/main.rs
@@ -1,19 +1,9 @@
 #![feature(test)]
 
-mod day08;
+mod day09;
 
 fn main() {
-    let input = aoc_utils::get_input(2023, 08);
-    // let input = "LR
-
-    // AAA = (AAB, XXX)
-    // AAB = (XXX, AAZ)
-    // AAZ = (AAB, XXX)
-    // BBA = (BBB, XXX)
-    // BBB = (BBC, BBC)
-    // BBC = (BBZ, BBZ)
-    // BBZ = (BBB, BBB)
-    // XXX = (XXX, XXX)";
-    day08::solve_a(&input);
-    day08::solve_b(&input);
+    let input = aoc_utils::get_input(2023, 09);
+    day09::solve_a(&input);
+    day09::solve_b(&input);
 }
