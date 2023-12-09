a481bfdaec03dc4ec03bfe14ffa5776ab6e3b233
Samuel Recker <samuel.recker@gmail.com>
2023-12-09 21:22:55 +0100

    fix name

--- rust/2023/src/main.rs
+++ rust/2023/src/main.rs
@@ -1,9 +1,9 @@
 #![feature(test)]
 
-mod day09_legendre;
+mod day09_lagrange;
 
 fn main() {
     let input = aoc_utils::get_input(2023, 09);
-    day09_legendre::solve_a(&input);
-    day09_legendre::solve_b(&input);
+    day09_lagrange::solve_a(&input);
+    day09_lagrange::solve_b(&input);
 }
