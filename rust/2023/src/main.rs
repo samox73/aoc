730f8267ab63d9d2a8408571ff14c46f3264b953
Samuel Recker <samuel.recker@gmail.com>
2023-12-22 21:45:02 +0100

    FUUUUCK

--- rust/2023/src/main.rs
+++ rust/2023/src/main.rs
@@ -4,6 +4,11 @@ mod day20;
 
 fn main() {
     let input = aoc_utils::get_input(2023, 20);
+    //     let input = "broadcaster -> a
+    // %a -> inv, con
+    // &inv -> b
+    // %b -> con
+    // &con -> output";
     day20::solve_a(&input);
     day20::solve_b(&input);
 }
