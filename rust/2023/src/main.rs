4dddb9c0e4b5d23aaaae2a69a06f1bf5888a6a15
Samuel Recker <samuel.recker@gmail.com>
2023-12-28 16:25:18 +0100

    !!! RUST: 2023 !!!

--- rust/2023/src/main.rs
+++ rust/2023/src/main.rs
@@ -1,14 +1,9 @@
 #![feature(test)]
 
-mod day24;
+mod day25;
 
 fn main() {
-    let input = aocutils::get_input(2023, 24);
-    //     let input = "19, 13, 30 @ -2, 1, -2
-    // 18, 19, 22 @ -1, -1, -2
-    // 20, 25, 34 @ -2, -2, -4
-    // 12, 31, 28 @ -1, -2, -1
-    // 20, 19, 15 @  1, -5, -3";
-    day24::solve_a(&input);
-    day24::solve_b(&input);
+    let input = aocutils::get_input(2023, 25);
+    day25::solve_a(&input);
+    day25::solve_b(&input);
 }
