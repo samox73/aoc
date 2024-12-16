a6e5d5f47226b6e11850d4e250cf4dfb5213e14a
Samuel Recker <samuel.recker@gmail.com>
2024-12-16 14:06:13 +0100

    reenter session cookie, if expired

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -5,10 +5,10 @@ mod utils;
 
 fn main() {
     let year = 2024;
-    let day = 08;
+    let day = 09;
     let input = utils::input::get(year, day);
     println!(" ----------------------------");
     println!(" |   AdventOfCode {}/{:0>2}   |", year, day);
     println!(" ----------------------------");
-    year2024::day08::solve(&input);
+    year2024::day09::solve(&input);
 }
--- rust/src/utils/input.rs
+++ rust/src/utils/input.rs
@@ -14,9 +14,16 @@ pub fn get(year: i16, day: i8) -> String {
             println!("could not find cached file, downloading...");
             let url = format!("https://adventofcode.com/{}/day/{}/input", year, day);
             let cookie = get_sessionid();
-            let input = get_content(&url, &cookie).expect("msg");
-            cache_input(&input, year, day);
-            input
+            return match get_content(&url, &cookie) {
+                Ok(input) => {
+                    cache_input(&input, year, day);
+                    input
+                }
+                Err(_) => {
+                    save_new_sessionid();
+                    return get(year, day);
+                }
+            };
         }
     };
 }
@@ -51,18 +58,23 @@ fn get_sessionid_file_path() -> PathBuf {
     get_cache_dir().join("sessionid")
 }
 
+fn save_new_sessionid() -> String {
+    let path = get_sessionid_file_path();
+    print!("please provide your session cookie (value of the header 'Cookie'): ");
+    io::stdout().flush().unwrap();
+    let mut line = String::new();
+    let _ = std::io::stdin().read_line(&mut line).unwrap();
+    fs::create_dir_all(get_cache_dir()).unwrap();
+    fs::write(path, &line).expect("unable to write session cookie to file file");
+    println!("session cookie successfully saved to file");
+    return line;
+}
+
 fn get_sessionid() -> String {
     let path = get_sessionid_file_path();
     let id = fs::read_to_string(&path);
     if id.is_err() {
-        print!("please provide your session cookie (value of the header 'Cookie'): ");
-        io::stdout().flush().unwrap();
-        let mut line = String::new();
-        let _ = std::io::stdin().read_line(&mut line).unwrap();
-        fs::create_dir_all(get_cache_dir()).unwrap();
-        fs::write(path, &line).expect("unable to write session cookie to file file");
-        println!("session cookie successfully saved to file");
-        return line;
+        return save_new_sessionid();
     }
     id.unwrap().trim().to_owned()
 }
